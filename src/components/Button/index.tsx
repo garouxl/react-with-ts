81603637c895ff73df8c3bae4cc5393f61e10e1b
Leandro Almeida <vempari@gmail.com>
2023-02-10 15:44:39 -0300

    refactor

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,10 @@
 import { IButton } from '../../types/types'
 import style from './Button.module.scss'
 
-const Button = ({
-  texto,
-  type = undefined,
-  action,
-}: IButton) => {
-  return (
-    <button type={type} className={style.button} onClick={action} >
-      {texto}
-    </button>
-  )
-}
+const Button = ({ texto, type = undefined, action }: IButton) => (
+  <button type={type} className={style.button} onClick={action}>
+    {texto}
+  </button>
+)
 
 export default Button
--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -6,34 +6,28 @@ interface Props extends ITasks {
   selectTask: (selectTask: ITasks) => void
 }
 
-const Item = ({ id, task, time, finished, selected, selectTask }: Props) => {
-  return (
-    <li
-      className={`${style.item} ${selected ? style.itemSelected : ''} ${
-        finished ? style.itemFinished : ''
-      }`.trim()}
-      onClick={() => {
-        if (!finished) {
-          selectTask({
-            id,
-            task,
-            time,
-            finished,
-            selected,
-          })
-        }
-      }}
-    >
-      <h3>
-        {id}: {task}
-      </h3>
-      <span>{time}</span>
-      <p>
-        {String(finished)} - {String(selected)}
-      </p>
-      <div className={style.finished}></div>
-    </li>
-  )
-}
+const Item = ({ id, task, time, finished, selected, selectTask }: Props) => (
+  <li
+    className={`${style.item} ${selected ? style.itemSelected : ''} ${
+      finished ? style.itemFinished : ''
+    }`.trim()}
+    onClick={() =>
+      !finished &&
+      selectTask({
+        id,
+        task,
+        time,
+        finished,
+        selected,
+      })
+    }
+  >
+    <h3>
+      {id}: {task}
+    </h3>
+    <span>{time}</span>
+    {finished && <div className={style.finished}></div>}
+  </li>
+)
 
 export default Item
